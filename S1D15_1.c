a4cffd92f7ba9994e18a7125dfa8ba53777cb766
anubhavvs <anubhavvsaha2001@gmail.com>
2019-10-17 20:51:32 +0530

    Day 15 Assignment

---
+++ S1D15_1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char x[20];
+	int i;
+	printf("Enter any name:\n");
+	gets(x);
+	printf("\nResult:\n");
+	printf("%c.",toupper(x[0]));
+	for(i=1;i<strlen(x);i++)
+	{
+		if(x[i]==' ')
+		{
+			printf("%c.",toupper(x[i+1]));
+		}
+	}
+	printf("\n");
+	return 0;
+}
---
+++ S1D15_2.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char x[20];
+	int i,r;
+	printf("Enter any name:\n");
+	gets(x);
+	printf("\nResult:\n");
+	for(i=strlen(x);i>=0;i--)
+	{
+		if(x[i]==' ')
+		{
+			r = i;
+			break;
+		}
+	}
+	printf("%c.",toupper(x[0]));
+	for(i=1;i<r;i++)
+	{
+		if(x[i]==' ')
+		{
+			printf("%c.",toupper(x[i+1]));
+		}
+	}
+	for(i=r+1;i<strlen(x);i++)
+	{
+		if(i==r+1)
+		{
+			printf("%c",toupper(x[i]));
+		}
+		else
+		{
+			printf("%c",tolower(x[i]));
+		}
+	}
+	printf("\n");
+	return 0;
+}
