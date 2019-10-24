ad704c14a69d292f7b2ab780e0a1a83980770541
Anubhav Saha <45201547+anubhavvs@users.noreply.github.com>
2019-10-24 13:58:24 +0530

    Day 6 Assignment

---
+++ SEMESTER 1/S1D6_1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	for(i=1;i<=10;i++)
+	{
+		printf("Good Morning\n");
+	}
+}
+
+/*OUTPUT:
+	Good Morning
+	Good Morning
+	Good Morning
+	Good Morning
+	Good Morning
+	Good Morning
+	Good Morning
+	Good Morning
+	Good Morning
+	Good Morning
+*/
