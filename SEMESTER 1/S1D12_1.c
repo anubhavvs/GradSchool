2983f1e50e6a1c45371540a90b5c6ab4cb7061ac
Anubhav Saha <45201547+anubhavvs@users.noreply.github.com>
2019-10-24 17:19:47 +0530

    Day 12 Assignment

--- SEMESTER 1/S1D12_1.c
+++ SEMESTER 1/S1D12_1.c
@@ -2,9 +2,9 @@
 int main()
 {
 	int x[10],i;
+	printf("Enter 10 integer values:\n");
 	for(i=0;i<10;i++)
 	{
-		printf("Enter a value: ");
 		scanf("%d",&x[i]);
 	}
 	printf("Reversed Order:\n");
@@ -14,3 +14,26 @@ int main()
 	}
 	return 0;
 }
+
+/*OUTPUT:
+	Enter 10 integer values:
+	12
+	32
+	56
+	98
+	54
+	78
+	14
+	23
+	10
+	Reversed Order:
+	10
+	23
+	14
+	78
+	54
+	98
+	56
+	32
+	12
+*/
