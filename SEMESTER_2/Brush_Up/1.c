61a074e534725d3ccf987cfe8071b8c4db5c1dd8
anubhavvs <anubhavvsaha2001@gmail.com>
2020-01-20 22:30:16 +0530

    Brush Up Assignment 1

--- SEMESTER_2/Brush_Up/1.c
+++ SEMESTER_2/Brush_Up/1.c
@@ -1,1 +1,63 @@
 /* Write a C program to take two 10-elements array, consider user input and display them using function */
+
+#include<stdio.h>
+void getarray(int a[10])
+{
+	int i;
+	for(i=0;i<10;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+}
+void printarray(int a[10])
+{
+	int i;
+	for(i=0;i<10;i++)
+	{
+		printf("%d, ",a[i]);
+	}
+}
+int main()
+{
+	int x[10], y[10];
+	printf("Enter the first array:\n");
+	getarray(x);
+	printf("Enter the second array:\n");
+	getarray(y);
+	printf("\nFirst array:\n");
+	printarray(x);
+	printf("\nSecond array:\n");
+	printarray(y);
+	return 0;
+}
+
+/* OUTPUT :
+	Enter the first array:
+	1
+	2
+	3
+	4
+	5
+	6
+	7
+	8
+	9
+	10
+	Enter the second array:
+	10
+	9
+	8
+	7
+	6
+	5
+	4
+	3
+	2
+	1
+	
+	First array:
+	1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+	Second array:
+	10, 9, 8, 7, 6, 5, 4, 3, 2, 1,
+*/
+
