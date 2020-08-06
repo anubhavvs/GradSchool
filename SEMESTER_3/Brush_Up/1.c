ebd43439027ef16b2d7ca68cfb71c147ba0b7c4c
anubhavvs <anubhavvsaha2001@gmail.com>
2020-08-06 14:28:30 +0530

    Brush Up Assignment 1

---
+++ SEMESTER_3/Brush_Up/1.c
@@ -0,0 +1,47 @@
+#define N 10
+#include<stdio.h>
+
+struct student {
+	char name[20];
+	int roll;
+	int marks[5];
+	int sum;
+};
+
+void input(struct student st[N])
+{
+	int i,j;
+	for(i=0;i<N;i++)
+	{
+		st[i].sum=0;
+		printf("Enter the details for %d student:\n",i+1);
+		printf("Name: ");
+		scanf("%s",&st[i].name);
+		printf("Roll: ");
+		scanf("%d",&st[i].roll);
+		printf("Enter marks:\n");
+		for(j=0;j<5;j++)
+		{
+			scanf("%d",&st[i].marks[j]);
+			st[i].sum += st[i].marks[j];
+		}
+	}
+}
+
+int main()
+{
+	struct student st[N];
+	struct student topper;
+	input(st);
+	int i,j, max=0;
+	for(i=0;i<N;i++)
+	{
+		if(st[i].sum >= max)
+		{
+			max = st[i].sum;
+			topper = st[i];
+		}
+	}
+	printf("Topper:\nName: %s\nMarks: %d\n",topper.name,topper.sum);
+	return 0;
+}
